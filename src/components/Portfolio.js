96795c2174f7728577ffb763464a2aea99aebb35
sot26 <omotoshotobi26@gmail.com>
2023-10-08 19:17:01 +0100

    portfolio section update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -30,7 +30,7 @@ const Portfolio = () => {
           <div className="w-full md:w-auto my-3 md:my-9">
             <iframe
               className="w-full h-[400px] md:w-[700px] lg:w-[880px] sm:h-[500px] "
-              src="https://www.youtube.com/embed/6FsWExmENe0?si=a5HftSiknQVkT6hA"
+              src="https://www.youtube.com/embed/FDlrkFs84eo?si=Xv16bt0atIJ3PKmD"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
