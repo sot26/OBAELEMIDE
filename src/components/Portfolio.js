5b06858b13b4ed81e77c12bec220eb5e0b2f5595
sot26 <omotoshotobi26@gmail.com>
2023-12-19 19:53:48 +0100

    button effect

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -32,12 +32,15 @@ const Portfolio = () => {
                 href="https://drive.google.com/drive/folders/1-0IgtbYwBbhkdfkds0BUmhHPy1CgScks"
                 className="flex justify-center items-center"
               >
-                <motion.button
-                  className="p-2 mt-3 text-lg  border-2 rounded-xl border-[#147EFB] bg-[#147EFB] text-white"
-                  variants={buttonVariant}
-                  animate="hover"
-                >
-                  View more >>>
+                <motion.button variants={buttonVariant} animate="hover">
+                  <button class="relative border hover:border-sky-600 mt-3 duration-500 group cursor-pointer text-sky-50  overflow-hidden h-12 w-auto px-6 rounded-md text-lg bg-sky-800  flex justify-center items-center ">
+                    <div className="absolute z-10 w-48 h-48 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-900 delay-150 group-hover:delay-75"></div>
+                    <div className="absolute z-10 w-40 h-40 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-800 delay-150 group-hover:delay-100"></div>
+                    <div className="absolute z-10 w-32 h-32 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-700 delay-150 group-hover:delay-150"></div>
+                    <div className="absolute z-10 w-24 h-24 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-600 delay-150 group-hover:delay-200"></div>
+                    <div className="absolute z-10 w-16 h-16 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-500 delay-150 group-hover:delay-300"></div>
+                    <p className="z-10">View More >>></p>
+                  </button>
                 </motion.button>
               </a>
             </p>
@@ -99,12 +102,22 @@ const Portfolio = () => {
             href="https://drive.google.com/drive/folders/1-0IgtbYwBbhkdfkds0BUmhHPy1CgScks"
             className="flex justify-center items-center"
           >
-            <motion.button
+            {/* <motion.button
               className="p-2 my-3 text-lg  border-2 rounded-xl border-[#147EFB] bg-[#147EFB] text-white"
               variants={buttonVariant}
               animate="hover"
             >
               View more >>>
+            </motion.button> */}
+            <motion.button variants={buttonVariant} animate="hover">
+              <button class="relative border hover:border-sky-600 mt-3 duration-500 group cursor-pointer text-sky-50  overflow-hidden h-12 w-auto px-6 rounded-md text-lg bg-sky-800  flex justify-center items-center ">
+                <div className="absolute z-10 w-48 h-48 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-900 delay-150 group-hover:delay-75"></div>
+                <div className="absolute z-10 w-40 h-40 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-800 delay-150 group-hover:delay-100"></div>
+                <div className="absolute z-10 w-32 h-32 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-700 delay-150 group-hover:delay-150"></div>
+                <div className="absolute z-10 w-24 h-24 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-600 delay-150 group-hover:delay-200"></div>
+                <div className="absolute z-10 w-16 h-16 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-sky-500 delay-150 group-hover:delay-300"></div>
+                <p className="z-10">View More >>></p>
+              </button>
             </motion.button>
           </a>
         </div>
