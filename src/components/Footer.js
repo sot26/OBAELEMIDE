b896d5e1967884963df67f3e1366a3da7b23ac56
sot26 <omotoshotobi26@gmail.com>
2023-12-31 12:35:04 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="w-full h-[120px] bg-[#f9f9f9]">
+    <div className="w-full h-[120px] ">
       <div className="flex justify-center items-center h-full gap-4 lg:gap-24 flex-col lg:flex-row px-[20px]">
         <div className="font-bold text-[17px] flex flex-wrap justify-center items-center text-center lg:text-left">
           Copyright © {currentYear}. All rights are reserved. OBAELEMIDE
